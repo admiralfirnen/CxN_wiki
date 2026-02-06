80d73c69a63ea844ab83af38d301b4fb5eccea80
admiralfirnen <sean.fw.mccluskey@gmail.com>
2026-02-05 21:13:40 -0700

    kingdom maps page

---
+++ serve.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8080;
+const MIME = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.ico': 'image/x-icon',
+  '.svg': 'image/svg+xml',
+  '.woff2': 'font/woff2',
+};
+
+const server = http.createServer((req, res) => {
+  let file = req.url === '/' ? '/index.html' : req.url;
+  file = path.join(__dirname, file.split('?')[0]);
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    const ext = path.extname(file);
+    res.setHeader('Content-Type', MIME[ext] || 'application/octet-stream');
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('CxN Wiki at http://localhost:' + PORT);
+  console.log('Login: http://localhost:' + PORT + '/login.html');
+});
